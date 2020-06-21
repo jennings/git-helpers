5bb4f2b0b04ba86bc6a1d1e57135e04ad23e4b8c
Stephen Jennings <stephen.g.jennings@gmail.com>
2020-06-21 13:58:39 -0700

    add tests for pull request URLs

--- git_helpers.py
+++ git_helpers.py
@@ -1,26 +1,52 @@
 import re
 import sys
+from dataclasses import dataclass
 
-def get_base_url(remote):
+@dataclass
+class Repo:
+    home_url: str
+    service: str
+
+    def new_pr(self, branch):
+        if self.service == "github":
+            return self.home_url + "/pull/new/" + branch
+        elif self.service == "bitbucket":
+            return self.home_url + "/pull-requests/new?source=" + branch
+        else:
+            return None
+
+def parse_remote_url(remote):
     if remote.startswith("git@github.com:"):
         match = re.search(r"^git@github.com:(.+)/(.*?)(?:\.git)?(/|$)", remote)
         if not match: sys.exit(f"did not match GitHub SSH remote: {remote}")
-        return f"https://github.com/{match.group(1)}/{match.group(2)}", "github"
+        return Repo(
+            f"https://github.com/{match.group(1)}/{match.group(2)}",
+            "github"
+        )
 
     elif remote.startswith("git@bitbucket.org:"):
         match = re.search(r"^git@bitbucket.org:(.+)/(.*?)(?:\.git)?(/|$)", remote)
         if not match: sys.exit(f"did not match Bitbucket SSH remote: {remote}")
-        return f"https://bitbucket.org/{match.group(1)}/{match.group(2)}", "bitbucket"
+        return Repo(
+            f"https://bitbucket.org/{match.group(1)}/{match.group(2)}",
+            "bitbucket"
+        )
 
     elif remote.startswith("https://github.com/"):
         match = re.search(r"^https://github.com/(.+)/(.*?)(?:\.git)?(/|$)", remote)
         if not match: sys.exit(f"did not match GitHub HTTPS remote: {remote}")
-        return f"https://github.com/{match.group(1)}/{match.group(2)}", "github"
+        return Repo(
+            f"https://github.com/{match.group(1)}/{match.group(2)}",
+            "github"
+        )
 
     elif remote.startswith("https://bitbucket.org/"):
         match = re.search(r"^https://bitbucket.org/(.+)/(.*?)(?:\.git)?(/|$)", remote)
         if not match: sys.exit(f"did not match Bitbucket HTTPS remote: {remote}")
-        return f"https://bitbucket.org/{match.group(1)}/{match.group(2)}", "bitbucket"
+        return Repo(
+            f"https://bitbucket.org/{match.group(1)}/{match.group(2)}",
+            "bitbucket"
+        )
 
     else:
         sys.exit(f"unknown URL format: {remote}")
--- git_helpers_test.py
+++ git_helpers_test.py
@@ -1,12 +1,12 @@
 import unittest
-from git_helpers import get_base_url
+from git_helpers import parse_remote_url
 
 class GitUpstreamUrl(unittest.TestCase):
     def test_https_urls(self):
-        def test(remote, expected_url, expected_host):
-            base_url, host = get_base_url(remote)
-            self.assertEqual(base_url, expected_url)
-            self.assertEqual(host, expected_host)
+        def test(remote, expected_url, expected_service):
+            repo = parse_remote_url(remote)
+            self.assertEqual(repo.home_url, expected_url)
+            self.assertEqual(repo.service, expected_service)
 
         test("https://github.com/jennings/example", "https://github.com/jennings/example", "github")
         test("https://github.com/jennings/example.git", "https://github.com/jennings/example", "github")
@@ -14,15 +14,23 @@ class GitUpstreamUrl(unittest.TestCase):
         test("https://bitbucket.org/jennings/example.git", "https://bitbucket.org/jennings/example", "bitbucket")
 
     def test_ssh_urls(self):
-        def test(remote, expected_url, expected_host):
-            base_url, host = get_base_url(remote)
-            self.assertEqual(base_url, expected_url)
-            self.assertEqual(host, expected_host)
+        def test(remote, expected_url, expected_service):
+            repo = parse_remote_url(remote)
+            self.assertEqual(repo.home_url, expected_url)
+            self.assertEqual(repo.service, expected_service)
 
         test("git@github.com:jennings/example", "https://github.com/jennings/example", "github")
         test("git@github.com:jennings/example.git", "https://github.com/jennings/example", "github")
         test("git@bitbucket.org:jennings/example", "https://bitbucket.org/jennings/example", "bitbucket")
         test("git@bitbucket.org:jennings/example.git", "https://bitbucket.org/jennings/example", "bitbucket")
+
+    def test_pr_url(self):
+        def test(remote, branch, expected_url):
+            repo = parse_remote_url(remote)
+            self.assertEqual(repo.new_pr(branch), expected_url)
+
+        test("git@github.com:jennings/example", "foo", "https://github.com/jennings/example/pull/new/foo")
+        test("git@bitbucket.org:jennings/example", "foo", "https://bitbucket.org/jennings/example/pull-requests/new?source=foo")
 
 if __name__ == "__main__":
     unittest.main()
